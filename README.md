6f373d252ae8c5b445f2749d79bb53f3c8ccbc5c
Ram <vaishnav.rd@gmail.com>
2018-12-31 20:07:54 +0530

    updated content script to dynamically add buttons for dynamically added suggestions later;

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 A cross browser add-on to enable 'play next' option in videos suggestions to play automatically after current video is finished playing.
 
 # Firefox listing link
-https://addons.mozilla.org/
+https://addons.mozilla.org/en-US/firefox/addon/youtube-playnext-queue/
 
 # Description
-* This add-on adds extra button on youtube player to enable 'play next' option in suggested videos
+* This add-on adds extra button on youtube player to enable 'play next' option in suggested videos.
+* If auto play is not working fine, you will either disable "Block websites from automatically playing media with sound" option or add youtube as exception to it.
 * Enjoy seamless experience from this very light weight add-on very.
 * This addon is completely safe to use.
-* This is an open source project, find the code on https://github.com/gurumukhi/.
+* This is an open source project, find the code on https://github.com/gurumukhi/youtube-queue-addon.
 
 Special Feature
 * This add-on supports verbose/debug mode on option page. This mode enables verbose logging in the browser console.
 
 Support:
-* Kindly feel free to open an issue on https://github.com/gurumukhi/ for any support.
+* Kindly feel free to open an issue on https://github.com/gurumukhi/youtube-queue-addon/issues for any support.
--- content_script.js
+++ content_script.js
@@ -1,53 +1,34 @@
-/* ToDos:
-  1. Better button
-  2. Better button placement
-  3. Add button to right side playlist videos too
-  4. Enable auto play on navigating to next
-  5. ..
-
-  Debug / corner cases:
-  1. How is it behaving with AutoPlay on
-*/
-
 var loggingEnabled = false;
 var initCalled = false;
 
-addButtonOnYoutubePlayer = function () {
-  var i = document.createElement('input');
-  i.type = "image";
-  i.src = "https://static.thenounproject.com/png/93129-200.png";
-  i.id = 'playNext';
-  i.classList.add('playNextAddonBtn');
-  i.title = "Play this video next";
+var i = document.createElement('input');
+i.type = "image";
+i.src = "https://static.thenounproject.com/png/93129-200.png";
+i.id = 'playNext';
+i.classList.add('playNextAddonBtn');
+i.title = "Play this video next";
 
+addedCSSOn = [];
+
+addButtonsIfRequired = function () {
   for (j=0; j < document.querySelectorAll('ytd-compact-video-renderer').length; j++) {
-    console.log(document.querySelectorAll('ytd-video-meta-block')[j]);
     ii = i.cloneNode();
     ii.id += j;
-    console.log(ii.id);
-    // document.querySelectorAll('ytd-video-meta-block')[j].appendChild(ii);
-    // console.log(document.querySelectorAll('ytd-compact-video-renderer')[j]);
-    // document.querySelectorAll('a.yt-simple-endpoint')[j].appendChild(i);
-    // document.querySelectorAll('span#video-title.style-scope.ytd-compact-video-renderer')[j].appendChild(i);
-    document.querySelectorAll('ytd-compact-video-renderer')[j].appendChild(ii);
+    if (document.querySelectorAll('ytd-compact-video-renderer')[j].querySelector('#'+ii.id)) {
+    } else {
+      document.querySelectorAll('ytd-compact-video-renderer')[j].appendChild(ii);
+      ii.removeEventListener('click', clickAction);
+      ii.addEventListener('click', clickAction);
+    }
   }
-  document.querySelectorAll('.playNextAddonBtn').forEach(b => {
-    b.addEventListener('click', (e) => {
-      setPlayNextURL(b.closest('ytd-compact-video-renderer').querySelector('a').href);
-      currentActive = document.querySelector('.playNextAddonBtnActive');
-      if (currentActive) {
-        currentActive.classList.remove('playNextAddonBtnActive');
-        currentActive.title = "Play this video next";
-      }
-      b.classList.add('playNextAddonBtnActive');
-      b.title = "This video is added to queue to be played next.";
-      e.preventDefault();
-      e.stopPropagation();
-    });
-  });
-  v = document.querySelector('video');
-  v.addEventListener('ended', () => {console.log('ended current'); playNext();});
+  setTimeout(addButtonsIfRequired, 5000);
+};
 
+addCSS = function () {
+  if (addedCSSOn.indexOf(document.location.href) > -1) {
+    return;
+  }
+  addedCSSOn.push(document.location.href);
   var css = '';
   css += ".playNextAddonBtn:hover { opacity: 1; }";
   css += ".playNextAddonBtn { opacity: 0.4; margin-top: 30px; height: 20px; width: 20px; }";
@@ -56,6 +37,25 @@ addButtonOnYoutubePlayer = function () {
   style.type = 'text/css';
   style.innerHTML = css;
   document.getElementsByTagName('head')[0].appendChild(style);
+};
+
+addButtonOnYoutubePlayer = function () {
+  addButtonsIfRequired();
+  addCSS();
+  document.querySelector('video').addEventListener('ended', playNext);
+};
+
+clickAction = (e) => {
+  setPlayNextURL(e.target.closest('ytd-compact-video-renderer').querySelector('a').href);
+  currentActive = document.querySelector('.playNextAddonBtnActive');
+  if (currentActive) {
+    currentActive.classList.remove('playNextAddonBtnActive');
+    currentActive.title = "Play this video next";
+  }
+  e.target.classList.add('playNextAddonBtnActive');
+  e.target.title = "This video is added to queue to be played next.";
+  e.preventDefault();
+  e.stopPropagation();
 };
 
 setPlayNextURL = (url) => {
@@ -75,7 +75,6 @@ setPlayNextURL = (url) => {
 playNext = () => {
   var storageItem = browser.storage.local.get();
   storageItem.then((value) => {
-    console.log(value);
     if (value.youtubePlayNextURL) {
       browser.storage.local.set({
         youtubePlayNextURL  : "#"
@@ -94,17 +93,14 @@ addEventListener = function() {
 
 logger = function(message) {
   if (loggingEnabled) {
-    console.log(`Youtube Screenshot Addon: ${message}`);
   }
 };
 
 init = function () {
   if (initCalled) {
-    console.log('NOT SETTING');
     return;
   }
 
-  console.log('SETTING');
   initCalled = true;
   // Initialization
   var storageItem = browser.storage.local.get();
@@ -118,19 +114,4 @@ init = function () {
   });
 };
 
-autoPlay = () => {
-  console.log("AUTOPLAY CHECK");
-  var storageItem = browser.storage.local.get();
-  storageItem.then((value) => {
-    if (value.youtubePlayNextURL == '#') {
-      browser.storage.local.set({
-        youtubePlayNextURL  : ""
-      });
-      v = document.querySelector('video');
-      v.play();
-    }
-  });
-};
-
-setTimeout(autoPlay, 2000);
 setTimeout(init, 5000);
