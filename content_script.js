30075156eb80545cd94aa4b9901116294b51e285
Ram <vaishnav.rd@gmail.com>
2019-01-20 21:17:47 +0530

    1.5.0 version with dark theme bugfix

--- content_script.js
+++ content_script.js
@@ -3,45 +3,55 @@ var initCalled = false;
 
 var i = document.createElement('input');
 i.type = "image";
-i.src = "https://static.thenounproject.com/png/93129-200.png";
 i.id = 'playNext';
 i.classList.add('playNextAddonBtn');
 i.title = "Play this video next";
 
-addedCSSOn = [];
-
 addButtonsIfRequired = function () {
+  var isDarkThemeEnabled = document.querySelector('html').getAttribute('dark') == 'true';
+  logger('isDarkThemeEnabled? - ' + isDarkThemeEnabled);
+
   for (j=0; j < document.querySelectorAll('ytd-compact-video-renderer').length; j++) {
-    ii = i.cloneNode();
-    ii.id += j;
-    if (document.querySelectorAll('ytd-compact-video-renderer')[j].querySelector('#'+ii.id)) {
-    } else {
+    if (!document.querySelectorAll('ytd-compact-video-renderer')[j].querySelector('#'+'playNext'+j)) {
+      ii = i.cloneNode();
+      ii.id += j;
+      if (isDarkThemeEnabled) {
+        ii.src = browser.extension.getURL("icons/playnexticon-dark-theme.png");
+      } else {
+        ii.src = browser.extension.getURL("icons/playnexticon.png");
+      }
       document.querySelectorAll('ytd-compact-video-renderer')[j].appendChild(ii);
       ii.removeEventListener('click', clickAction);
       ii.addEventListener('click', clickAction);
     }
   }
-  setTimeout(addButtonsIfRequired, 5000);
-};
 
-addCSS = function () {
-  if (addedCSSOn.indexOf(document.location.href) > -1) {
-    return;
+  allBtns = document.querySelectorAll('input.playNextAddonBtn');
+  console.log('CHECKING image color issue');
+  if (allBtns[0].src == browser.extension.getURL("icons/playnexticon.png") && isDarkThemeEnabled) {
+    console.log('found white theme image in dark theme, fixing this');
+    // found white theme image in dark theme, fix this
+    allBtns.forEach(btn => {
+      btn.src = browser.extension.getURL("icons/playnexticon-dark-theme.png");
+      btn.classList.remove('whiteThemeBtn');
+      btn.classList.add('blackThemeBtn');
+    });
   }
-  addedCSSOn.push(document.location.href);
-  var css = '';
-  css += ".playNextAddonBtn:hover { opacity: 1; }";
-  css += ".playNextAddonBtn { opacity: 0.4; margin-top: 30px; height: 20px; width: 20px; }";
-  css += ".playNextAddonBtnActive { opacity: 1; border: 2px solid black; }";
-  var style = document.createElement('style');
-  style.type = 'text/css';
-  style.innerHTML = css;
-  document.getElementsByTagName('head')[0].appendChild(style);
+  else if (allBtns[0].src == browser.extension.getURL("icons/playnexticon-dark-theme.png") && !isDarkThemeEnabled) {
+    console.log('found dark theme image in white theme, fixing this');
+    // found dark theme image in white theme, fix this
+    allBtns.forEach(btn => {
+      btn.src = browser.extension.getURL("icons/playnexticon.png");
+      btn.classList.add('whiteThemeBtn');
+      btn.classList.remove('blackThemeBtn');
+    });
+  }
+
+  setTimeout(addButtonsIfRequired, 5000);
 };
 
 addButtonOnYoutubePlayer = function () {
   addButtonsIfRequired();
-  addCSS();
   document.querySelector('video').addEventListener('ended', playNext);
 };
 
@@ -77,7 +87,7 @@ playNext = () => {
   storageItem.then((value) => {
     if (value.youtubePlayNextURL) {
       browser.storage.local.set({
-        youtubePlayNextURL  : "#"
+        youtubePlayNextURL  : ""
       });
       window.location.href = value.youtubePlayNextURL;
     }
